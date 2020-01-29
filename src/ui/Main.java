878c20fe2d07cddf7ea2c4823825e27de3afb906
L3C74M0 <L3C74M0@gmail.com>
2020-01-29 16:46:58 -0500

    Nothing

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,19 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application{
-		
-		public static void main(String[] args) {
-			launch(args);
-		}
-		
-		public void start(Stage stage) throws Exception {
-			Parent root= FXMLLoader.load(getClass().getResource("MiniAgendaGUI.fxml"));
-	       	
-	       	Scene scene= new Scene(root);
-	       	stage.setTitle("Pacman");
-	       	stage.setScene(scene);
-	       	stage.show();
-			
-		}
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
 	}
+
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("MiniAgendaGUI.fxml"));
+
+		Scene scene = new Scene(root);
+		stage.setTitle("Agenda Electronica");
+		stage.setScene(scene);
+		stage.show();
+
+	}
+}
